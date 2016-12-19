da8655d65575b3e7a0dddbc745e723cd3c188504
lenz-alex <alexcgriff@gmail.com>
2016-12-19 13:22:15 -0800

    finished sections and tables working on jquery for more/less need to add lower table need to add hr to cover whole row

--- public_html/js/mindbodyjs.js
+++ public_html/js/mindbodyjs.js
@@ -9,12 +9,39 @@
     $( "#tabs" ).tabs();
   } );
   */
- 
+
+/* Business Overview Tab on load */
 $(document).ready(function(){
   activaTab('businessoverview');
 });
 
 function activaTab(tab){
-  $('.subnav a[href="#' + tab + '"]').tab('show');
+  $('.subnav a[href="#' + tab + '"]').tab('show').addClass('active');
 };
+
+
+/* have business overview show as ACTIVE upon load*/
+$('.subnav a').click(function(){
+    $('subnav a').removeClass('active');
+    $(this).addClass('active');
+});
+
+/*Less*/
+$(document).ready(function(){
+    $(".lessbutton").click(function(){
+        $(".more").hide(1000);
+        $(".morebutton").show(1000);
+        $(".lessbutton").hide(1000);
+    });
+});
+
+/*More*/
+$(document).ready(function(){
+    $(".morebutton").click(function(){
+        $(".more").show(1000);
+        $(".lessbutton").show(1000);
+        $(".morebutton").hide(1000);
+    });
+});
+
 
